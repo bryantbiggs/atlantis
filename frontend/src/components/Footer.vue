974fad4149cf08bf5453fd4f4fa6417c2bf5c6d3
Bryant Biggs <bryantbiggs@gmail.com>
2018-02-18 13:40:44 -0500

    Feature / Static Webage
    - Add Vuejs build configuration and files (using `vue-cli` and the command `vue init webpack`) to the `frontend` directory to start building out webpage
    - Update npm script in top level directory to include steps to;
        1. Build frontend files into `frontend/dist` directory
        2. Copy static `error.html` to `frontend/dist` directory
        3. Upload build files from `frontend/dist` to S3 bucket
        4. Deploy serverless application

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="main-footer">
+
+    <div class="container-wrapper md-layout-row">
+      <md-button class="md-raised" href="http://github.com/bryantbiggs/atlantis" target="_blank">Atlantis</md-button>
+      <md-button class="md-raised" href="http://github.com/bryantbiggs" target="_blank">Github</md-button>
+      <md-button class="md-raised" href="https://www.linkedin.com/in/bryantbiggs/" target="_blank">LinkedIn</md-button>
+    </div>
+
+    <div class="container-wrapper md-layout-row">
+      <p>All rights reserved Bryant Biggs 2018</p>
+    </div>
+
+  </footer>
+</template>
+
+
+<style scoped>
+  footer {
+    height: 132px;
+    padding: 2em;
+    position: relative;
+    color: #0b132b;
+    background-color: #fff;
+    font-family: 'Roboto Mono', monospace;
+  }
+
+  .md-layout-row {
+    text-align: center;
+  }
+
+  .md-raised {
+    color: #0b132b;
+  }
+  
+</style>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
