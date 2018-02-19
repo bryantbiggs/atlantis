22f9131f9d9a02a0e993b7fc4c8c29c885928608
Bryant Biggs <bryantbiggs@gmail.com>
2018-02-19 10:42:46 -0500

    Feature / Database Creation
    - Add shell script to provision read/write and read users & groups for a new PostgreSQL database instance
    - Add sql migration file to provision new db instance with a `subscriptions` shchema and three tables for
    `customer`, `product`, and `sales`
    - Add shell script to `.travis/` directory for creating local database instance inside of TravisCI
    - Update `.travis.yml` and `serverless.yml` to include new environment variables introduced in latest changes

---
+++ backend/db/migrations/0001_create_db.sql
@@ -0,0 +1,59 @@
+CREATE SCHEMA IF NOT EXISTS subscriptions AUTHORIZATION grp_crud;
+COMMENT ON SCHEMA subscriptions IS 'eCommerce Data - online and phone based subscription products';
+
+-- All Access
+ALTER DEFAULT PRIVILEGES FOR ROLE grp_crud, superadmin IN SCHEMA subscriptions GRANT ALL ON TABLES TO grp_crud;
+ALTER DEFAULT PRIVILEGES FOR ROLE grp_crud, superadmin IN SCHEMA subscriptions GRANT ALL ON FUNCTIONS TO grp_crud;
+ALTER DEFAULT PRIVILEGES FOR ROLE grp_crud, superadmin IN SCHEMA subscriptions GRANT ALL ON SEQUENCES TO grp_crud;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA subscriptions TO grp_crud;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA subscriptions TO grp_crud;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA subscriptions TO grp_crud;
+
+-- Read Access
+ALTER DEFAULT PRIVILEGES FOR ROLE grp_crud, superadmin IN SCHEMA subscriptions GRANT SELECT ON TABLES TO grp_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA subscriptions TO grp_read;
+
+-- Table creation
+BEGIN;
+    SET ROLE grp_crud;
+    SET search_path = subscriptions;
+
+
+    -- Customer table to capture each customer and their contact information (dimension table)
+    CREATE TABLE customer (
+        id INTEGER NOT NULL PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        street_address TEXT,
+        state TEXT,
+        zip_code TEXT
+    );
+    CREATE UNIQUE INDEX customerid_idx ON customer (id);
+    COMMENT ON TABLE customer IS 'Customer contact information';
+
+
+    -- Product table to capture each prodct and its cost (dimension table)
+    CREATE TABLE product (
+        id INTEGER NOT NULL PRIMARY KEY,
+        product_name VARCHAR(100),
+        cost DECIMAL(12,2)
+    );
+    CREATE UNIQUE INDEX productid_idx ON product (id);
+    COMMENT ON TABLE product IS 'Products for purchase';
+
+
+    -- Sales table to capture transactions over time as they are processed (fact table)
+    CREATE TYPE status AS ENUM ('new', 'canceled');
+    CREATE TABLE sales (
+        id SERIAL,
+        customer_id INTEGER NOT NULL REFERENCES customer(id),
+        product_id INTEGER NOT NULL REFERENCES product(id),
+        purchase_status status,
+        transaction_date TIMESTAMPTZ NOT NULL,
+        CONSTRAINT transaction UNIQUE (customer_id, product_id, purchase_status, transaction_date)
+    );
+    CREATE UNIQUE INDEX customerid_productid_idx ON sales (customer_id, product_id);
+    COMMENT ON TABLE sales IS 'Sales records';
+
+
+END;
