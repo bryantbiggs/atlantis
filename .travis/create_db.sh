28d76bcb5bdfe66e76026b427cac7ba4a76a5ecc
Bryant Biggs <bryantbiggs@gmail.com>
2019-11-16 16:26:42 -0500

    type(chore) Fix dependency update issues

--- .travis/create_db.sh
+++ .travis/create_db.sh
@@ -2,5 +2,5 @@
 
 # Required to setup known database values in TravisCI
 # Values set here are typically set through the AWS RDS console
-psql -c "CREATE DATABASE ${DB_NAME};"
-psql -c "CREATE ROLE ${DB_USERNAME} WITH SUPERUSER LOGIN PASSWORD '${DB_PASSWORD}';"
+psql -c "CREATE DATABASE ${DB_NAME};" -U postgres
+psql -c "CREATE USER ${DB_USERNAME} WITH SUPERUSER LOGIN PASSWORD '${DB_PASSWORD}';" -U postgres
